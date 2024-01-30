fab97b3c8c0b7ead84d6cf7bcb821d8a4e2d46c9
Georg Semmler <georg.semmler@giga-infosystems.com>
2024-01-30 09:01:48 +0100

    Make driver support configurable at build time

--- gdal-src/src/lib.rs
+++ gdal-src/src/lib.rs
@@ -1,2 +1,12 @@
+#[cfg(feature = "DRIVER_NETCDF")]
+extern crate netcdf_src;
+#[cfg(feature = "DRIVER_HDF5")]
+extern crate hdf5_src;
+#[cfg(feature = "DRIVER_SQLITE")]
 extern crate libsqlite3_sys;
+#[cfg(feature = "curl-sys")]
+extern crate curl_sys;
+#[cfg(feature = "DRIVER_PG")]
+extern crate pq_src;
+
 extern crate proj_sys;
